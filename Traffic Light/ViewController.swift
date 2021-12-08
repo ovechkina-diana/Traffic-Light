603b6f2925644bb3cb6f79cbe4a34762dd12ec37
Ovechkina Diana <ovechkinadiana@yandex.ru>
2021-12-08 18:35:16 +0300

    Change light

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -8,46 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var redLightView: UIView!
-    @IBOutlet weak var yellowLightView: UIView!
-    @IBOutlet weak var greenLightView: UIView!
    
-    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLightView.layer.cornerRadius = 100
-        yellowLightView.layer.cornerRadius = 100
-        greenLightView.layer.cornerRadius = 100
+        redLightView.layer.cornerRadius = 77
+        yellowLightView.layer.cornerRadius = 77
+        greenLightView.layer.cornerRadius = 77
     }
 
-    @IBAction func startButtonPressed() {
-        while (true) {
+
+    @IBAction func startPressed() {
+        
+        if (redLightView.alpha == 0.3) {
             redLightView.alpha = 1
-            
-            if (redLightView.alpha == 1) {
-                yellowLightView.alpha = 0.3
-                greenLightView.alpha = 0.3
-                startButton.setTitle("Next", for: .normal)
-            }
+        } else if (redLightView.alpha == 1) {
             redLightView.alpha = 0.3
             yellowLightView.alpha = 1
-            
-             if (yellowLightView.alpha == 1) {
-                redLightView.alpha = 0.3
-                greenLightView.alpha = 0.3
-                startButton.setTitle("Next", for: .normal)
-            }
-            
+            startButton.setTitle("Next", for: .normal)
+        } else if (yellowLightView.alpha == 1) {
             yellowLightView.alpha = 0.3
             greenLightView.alpha = 1
-            
-            if (greenLightView.alpha == 1) {
-                redLightView.alpha = 0.3
-                yellowLightView.alpha = 0.3
-                startButton.setTitle("Next", for: .normal)
-            }
-        }
+            startButton.setTitle("Next", for: .normal)
+        } else {
+            redLightView.alpha = 1
+            greenLightView.alpha = 0.3
+            startButton.setTitle("Next", for: .normal)
+
+   }
+        
+        
+
+        
     }
     
     

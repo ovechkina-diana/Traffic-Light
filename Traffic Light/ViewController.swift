9e51fa77be1bbdb326c733e1ca18e4ce62756489
Ovechkina Diana <ovechkinadiana@yandex.ru>
2021-12-08 12:58:58 +0300

    Initial Commit

---
+++ Traffic Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic Light
+//
+//  Created by Диана Овечкина on 08.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

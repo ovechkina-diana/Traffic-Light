b6bff4bcd60ef775e2be4e2ded6052a8e113d56b
Ovechkina Diana <ovechkinadiana@yandex.ru>
2021-12-08 14:46:46 +0300

    traffic lights

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var startButton: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 100
+        yellowLightView.layer.cornerRadius = 100
+        greenLightView.layer.cornerRadius = 100
     }
 
-
+    
+    
 }
 
